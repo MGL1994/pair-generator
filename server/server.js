a1a929947bc03546cde0c4aa8eaa8f6f930a0d89
MGL1994 <michaelgl1994@gmail.com>
2020-08-06 17:41:37 +0100

    Added individual cohort route

--- server/server.js
+++ server/server.js
@@ -8,23 +8,40 @@ app.use(bodyParser.json())
 
 const port = 3000
 
-const students = [
-    {name: "T.J."},
-    {name: "Vince"}, 
-    {name: "Spinelli"},
-    {name: "Gretchen"},
-    {name: "Gus"},
-    {name: "Mikey"}
+const cohorts = [
+    {
+        id: 0,
+        name: "Third Street", 
+        students: [
+            {name: "TJ"},
+            {name: "Vince"}, 
+            {name: "Spinelli"},
+            {name: "Gretchen"},
+            {name: "Gus"},
+            {name: "Mikey"}
+        ]
+    }
 ]
+
+// ROOT ROUTE
 
 app.get('/', (req, res) => res.send('TEEJAY!!!'))
 
-app.get('/students', (req, res) => res.send(JSON.stringify(students)))
+// COHORTS ROUTES
 
-app.post('/students', (req, res) => {
-    const newStudent = req.body
-    students.push(newStudent)
-    res.send({message: `${newStudent.name} has been added to the register.`})
+app.get('/cohorts', (req, res) => res.send(JSON.stringify(cohorts)))
+
+app.post('/cohorts', (req, res) => {
+    const newCohort = req.body
+    cohorts.push(newCohort)
+    res.send({message: `${newCohort.name} has been added to the register.`})
+})
+
+// INDV COHORT ROUTES
+
+app.get('/cohorts/:id', (req, res) => {
+    const id = req.params.id
+    res.send(JSON.stringify(cohorts[id]))
 })
 
 app.listen(port, () => console.log(`Now you can generate ${port} pairs!`))
