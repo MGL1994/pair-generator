aa22e9eca6842f46ede77e8cb7b88693cb463147
MGL1994 <michaelgl1994@gmail.com>
2020-08-11 11:46:31 +0100

    Fixed add cohort route

--- server/server.js
+++ server/server.js
@@ -19,85 +19,78 @@ const cohorts = [
             {name: "Gretchen"},
             {name: "Gus"},
             {name: "Mikey"}
-        ],
-        allPairs:[ 
-            {
-                pairInstance: 0,
-                pairs: [
-                    {pair: ["TJ", "Vince"]},
-                    {pair: ["Spinelli", "Gretchen"]},
-                    {pair: ["Gus", "Mikey"]}
-                ]
-            },
-            {
-                pairInstance: 1,
-                pairs: [
-                    {pair: ["TJ", "Mikey"]},
-                    {pair: ["Vince", "Spinelli"]},
-                    {pair: ["Gretchen", "Gus"]}
-                ]
-            },
         ]
+        // allPairs:[ 
+        //     {
+        //         pairInstance: 0,
+        //         pairs: [
+        //             {pair: ["TJ", "Vince"]},
+        //             {pair: ["Spinelli", "Gretchen"]},
+        //             {pair: ["Gus", "Mikey"]}
+        //         ]
+        //     },
+        //     {
+        //         pairInstance: 1,
+        //         pairs: [
+        //             {pair: ["TJ", "Mikey"]},
+        //             {pair: ["Vince", "Spinelli"]},
+        //             {pair: ["Gretchen", "Gus"]}
+        //         ]
+        //     }
+        // ]
     }
 ]
 
-function pairStudents(id) {
+// function pairStudents(id) {
 
-    const cohort = cohorts[id]
-    let students = cohort.students
-    let prevPairInstance = cohort.allPairs[cohort.allPairs.length - 1].pairInstance
-    let existingPairs = cohort.allPairs
+//     const cohort = cohorts[id]
+//     let students = cohort.students
+//     let prevPairInstance = cohort.allPairs[cohort.allPairs.length - 1].pairInstance
+//     let existingPairs = cohort.allPairs
 
-    // CHECK AMOUNT OF PAIRS
+//     // CHECK AMOUNT OF PAIRS
 
-    if(existingPairs.length === (students.length - 2)) {
-        existingPairs = []
-        prevPairInstance = (-1)
-    }
+//     // REORDER STUDENTS LIST
 
-    // REORDER STUDENTS LIST
+//     let duplicatesCount = 1
 
-    let duplicatesCount = 1
+//     while(duplicatesCount > 0) {
 
-    while(duplicatesCount > 0) {
+//         duplicatesCount = 0
 
-        duplicatesCount = 0
+//         for(let counter = students.length - 1; counter > 0; counter--) {
+//             const randomCounter = Math.floor(Math.random() * counter)
+//             const tempStudentsArray = students[counter]
+//             students[counter] = students[randomCounter]
+//             students[randomCounter] = tempStudentsArray
+//         }
 
-        for(let counter = students.length - 1; counter > 0; counter--) {
-            const randomCounter = Math.floor(Math.random() * counter)
-            const tempStudentsArray = students[counter]
-            students[counter] = students[randomCounter]
-            students[randomCounter] = tempStudentsArray
-        }
+//         // CHECK IF PAIRINGS EXIST
 
-        // CHECK IF PAIRINGS EXIST
+//         for(let counter = 0; counter < students.length; counter += 2) {
+//             existingPairs.map(pairInstance => {
+//                 pairInstance.pairs.map(pair => {
+//                     if(pair.pair.includes(students[counter].name) && pair.pair.includes(students[counter + 1].name)) {
+//                         duplicatesCount += 1
+//                     }
+//                 })
+//             })
+//         }
+//     }
 
-        for(let counter = 0; counter < students.length; counter += 2) {
-            existingPairs.map(pairInstance => {
-                pairInstance.pairs.map(pair => {
-                    if(pair.pair.includes(students[counter].name) && pair.pair.includes(students[counter + 1].name)) {
-                        duplicatesCount += 1
-                    }
-                })
-            })
-        }
-    }
+//     // CREATE NEW PAIRINGS
 
-    // CREATE NEW PAIRINGS
+//     cohort.allPairs.push({
+//         pairInstance: prevPairInstance + 1,
+//         pairs : [
+//             {pair: [students[0].name, students[1].name]},
+//             {pair: [students[2].name, students[3].name]},
+//             {pair: [students[4].name, students[5].name]},
+//         ]
+//     })
 
-    cohort.allPairs.push({
-        pairInstance: prevPairInstance + 1,
-        pairs : [
-            {pair: [students[0].name, students[1].name]},
-            {pair: [students[2].name, students[3].name]},
-            {pair: [students[4].name, students[5].name]},
-        ]
-    })
-
-    console.log(`existing pairs = ${existingPairs.length}`, `students = ${students.length - 2}`)
-
-    return existingPairs
-}
+//     return existingPairs
+// }
 
 // ROOT ROUTE
 
@@ -109,7 +102,10 @@ app.get('/cohorts', (req, res) => res.send(JSON.stringify(cohorts)))
 
 app.post('/cohorts', (req, res) => {
     const newCohort = req.body
-    cohorts.push(newCohort)
+    const id = cohorts.length
+    cohorts.push({
+        id: id, ...newCohort
+    })
     res.send({message: `${newCohort.name} has been added to the register.`})
 })
 
@@ -122,9 +118,11 @@ app.get('/cohorts/:id', (req, res) => {
 
 // PAIR ROUTES
 
-app.get('/cohorts/:id/pairs', (req, res) => {
-    const id = req.params.id
-    res.send(JSON.stringify(pairStudents(id)))
-})
+// app.get('/cohorts/:id/pairs', (req, res) => {
+//     const id = req.params.id
+//     res.send(JSON.stringify(pairStudents(id)))
+// })
+
+// SERVER
 
 app.listen(port, () => console.log(`Now you can generate ${port} pairs!`))
